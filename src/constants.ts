f26fa9792941c499cc20357341e96221974a44a8
Camillucci <nicolacamillucci@gmail.com>
2022-09-04 12:41:12 +0100

    Publishing update

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const ADD_NEW_COMMAND: string = "Add new command...";
 export const COMMAND_NAME: string = "run-command.run-command";
 export const CONFIGURATION_NAME: string = "run-command.commands";
 export const DEFAULT_PATH: string = "${workspaceFolder}/";
-export const STATUS_BAR_NAME: string = "$(terminal-view-icon) Run";
+export const STATUS_BAR_NAME: string = "$(terminal) Run";
